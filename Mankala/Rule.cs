493e1b20d6efd0076d27ecfe2078ae0697743f11
Jonathan van Dijk <jonathane.vandijk@gmail.com>
2023-11-07 15:39:37 +0100

    Nieuwe definities voor de oude structuur; nieuwe endgame regels; fix voor turn van mankala

--- Mankala/Rule.cs
+++ Mankala/Rule.cs
@@ -12,20 +12,24 @@ namespace Mankala
         public EndGameRule() { }
 
         public abstract bool GameIsEnded(Board b);
+
+        public abstract player Winner(Board b);
     }
 
     internal abstract class EndOfTurnRule
     {
         public EndOfTurnRule() { }
 
-        public abstract bool PlayerContinues(Board b, int lastPlace);
+        public abstract bool PlayerContinues(Board b, int lastPlace, player current);
 
-        public abstract void EndOfMove(Board b, int lastPlace);
+        public abstract void EndOfMove(Board b, int lastPlace, player current);
     }
 
     internal abstract class MoveRule
     {
         public MoveRule() { }
+
+        //clockwise through the array is ++ and anti-clockwise is --
         public abstract int Move(Board b, int start);
     }
 
@@ -34,19 +38,47 @@ namespace Mankala
 
     //              TurnRules
 
-    internal class StealRule : EndOfTurnRule
+    internal class MankalaEndGame : EndGameRule
+    {
+        public override bool GameIsEnded(Board b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override player Winner(Board b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class WariEndGame : EndGameRule
+    {
+        public override bool GameIsEnded(Board b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override player Winner(Board b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //              TurnRules
+
+    internal class MankalaTurn : EndOfTurnRule
     {
         //check for homebase
-        public override void EndOfMove(Board b, int endPlace)
+        public override void EndOfMove(Board b, int lastPlace, player current)
         {
             // !!! needs to end in player's pit
-            int endPitCount = b.pits[endPlace];
-            if (b.IsNonScoringPit(endPlace) && endPitCount == 1)
+            int endPitCount = b.pits[lastPlace];
+            if (b.IsNonScoringPit(lastPlace) && endPitCount == 1)
             {
-                int otherPit = OtherStealPit(b, endPlace);
-                b.PointsTo(b.pits[otherPit] + endPitCount);
+                int otherPit = OtherStealPit(b, lastPlace);
+                this.PointsTo(b, current, b.pits[otherPit] + endPitCount);
                 b.pits[otherPit] = 0;
-                b.pits[endPlace] = 0;
+                b.pits[lastPlace] = 0;
             }
         }
 
