2ceaaca55f0c5c10fd7ccf7e6b4ac9606c100c39
Jonathan van Dijk <jonathane.vandijk@gmail.com>
2023-11-17 21:34:25 +0100

    Rest of the turn functions + full output of the game It seems to work

--- Mankala/Program.cs
+++ Mankala/Program.cs
@@ -20,20 +20,20 @@ namespace Mankala
             //introduction
             Console.WriteLine("Welcome to the family of board games \"Mankala\"!!\n" +
                 "We have a choice of the following games:\n");
-            foreach (string name in InputHandler.madeGames.Keys)
+            foreach (string gameName in InputHandler.madeGames.Keys)
             {
-                Console.WriteLine(name);
+                Console.WriteLine(gameName);
             }
 
             //Game choice
             Console.WriteLine("Choose one to play:");
-            string game = Console.ReadLine().ToLower();
-            while (!InputHandler.madeGames.ContainsKey(game))
+            string name = Console.ReadLine().ToLower();
+            while (!InputHandler.madeGames.ContainsKey(name))
             {
-                Console.WriteLine("Sorry, we can't seem to retrieve: " + game);
-                game = Console.ReadLine().ToLower();
+                Console.WriteLine("Sorry, we can't seem to retrieve: " + name);
+                name = Console.ReadLine().ToLower();
             }
-            FamMankalaFact factory = InputHandler.madeGames[game];
+            FamMankalaFact factory = InputHandler.madeGames[name];
 
             //Creating a board
             Board gameBoard;
@@ -53,9 +53,9 @@ namespace Mankala
             else
             {//custom board choice
                 //Choosing amount of pits
-                Console.WriteLine("How big do you want the board?");
+                Console.WriteLine("How many pits should each player have?");
                 string inputPitCount = Console.ReadLine();
-                int pitCount;
+                int pitCount;//for one player/one side of the board
 
                 while (!InputHandler.AcceptedPitCount(inputPitCount, out pitCount))
                 {
@@ -71,7 +71,7 @@ namespace Mankala
                 {
                     inputStartAm = Console.ReadLine();
                 }
-                gameBoard = factory.bCreator.CreateBoard(pitCount, startAmount);
+                gameBoard = factory.bCreator.CreateBoard(pitCount * 2, startAmount);
             }
 
             //Printing board for the first time
@@ -87,17 +87,47 @@ namespace Mankala
                 Console.WriteLine("It is " + PlayerHandler.PlayerString(whoTurn) + "'s turn!");
                 (int, int) range = factory.moveRule.MoveRange(gameBoard, whoTurn);
                 Console.WriteLine("Numbered in anti-clockwise fashion starting left, you can choose pit: "
-                    + range.Item1 + "-" + range.Item2 + "of the pits on your side");
+                    + range.Item1 + "-" + range.Item2 + " of the pits on your side");
+
+                int lastPit;
                 int chosenMove;
                 string moveInput = Console.ReadLine();
+                //any hindrance has happened in doing the next move
                 bool unacceptable = true;
+
                 while (unacceptable)
                 {
                     if (InputHandler.AcceptedNumber(moveInput, out chosenMove))
                     {
                         if (factory.moveRule.AcceptableMove(gameBoard, chosenMove, whoTurn))
                         {
-                            unacceptable = false; //we can proceed
+                            try
+                            {
+                                lastPit = factory.moveRule.Move(gameBoard, chosenMove, whoTurn);
+                                Console.WriteLine("\n\nThe board before any special steps at the end of the turn:");
+                                Console.WriteLine(factory.bCreator.PrintBoard(gameBoard));//show the in between step
+
+                                //end the turn
+                                factory.endTurn.EndOfMove(gameBoard, lastPit, whoTurn);
+
+                                //deciding the next turn
+                                if (factory.endTurn.PlayerContinues(gameBoard, lastPit, whoTurn))
+                                    Console.WriteLine(PlayerHandler.PlayerString(whoTurn) + " gets another turn!");
+                                else//player changes
+                                    whoTurn = PlayerHandler.NextPlayer(whoTurn);
+
+                                //Show players the Board
+                                Console.WriteLine("The board after special rules:");
+                                Console.WriteLine(factory.bCreator.PrintBoard(gameBoard));
+
+                                unacceptable = false; //we can proceed
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                                Console.WriteLine(ex.StackTrace);
+                                Console.WriteLine("To retry, re-enter a move (this was a significant error/problem)");
+                            }
                         }
                         else
                         {
@@ -110,10 +140,27 @@ namespace Mankala
                      //handler already gives feedback
                         moveInput = Console.ReadLine();//update move, try again
                     }
-                }//Proceed here!!
+                }
+            }
+            int winnerNum = factory.endGameRule.Winner(gameBoard);
+            int[] pointDistribution = factory.endGameRule.Points(gameBoard);
+            if (winnerNum == 0)
+            {
+                Console.WriteLine("\n\n\nThe game ended in a tie");
+            }
+            else
+            {
+                Console.WriteLine("Player " + winnerNum + " has won the game with " + pointDistribution[winnerNum]
+                    + " points!");
+                string showDistr = "The points were: " + pointDistribution[1];
+                for (int i = 2; i < pointDistribution.Length; i++)
+                {
+                    showDistr += " : " + pointDistribution[i];
+                }
+                Console.WriteLine(showDistr);
             }
 
-
+            //keep the game running
             Console.ReadLine();
         }
     }
